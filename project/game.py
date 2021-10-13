0fa57f316bb4adc8eeea00d1871160472902d22c
puzzud <puzzud@gmail.com>
2021-10-13 15:28:27 -0400

    Added profiling.

--- project/game.py
+++ project/game.py
@@ -1,3 +1,5 @@
+from os import sys
+
 from chess.chessGameModel import ChessGameModel
 from gui.guiGameView import GuiGameView
 from gui.guiGameController import GuiGameController
@@ -15,4 +17,29 @@ def main() -> None:
 	guiGameController.loop()
 	gameModel.shutdown()
 
-main()
+if __name__ == "__main__":
+	profile = False
+
+	for arg in sys.argv[1:]:
+		argString: str = arg
+		if argString.lower() == "profile":
+			profile = True
+
+	if profile:
+		import cProfile
+
+		cProfileOutputFilename = "cProfileOutput.dat"
+		cProfile.run("main()", cProfileOutputFilename)
+
+		import pstats
+		from pstats import SortKey
+
+		with open("cProfileOutputTime.txt", "w") as file:
+			p = pstats.Stats(cProfileOutputFilename, stream = file)
+			p.sort_stats("time").print_stats()
+		
+		with open("cProfileOutputCalls.txt", "w") as file:
+			p = pstats.Stats(cProfileOutputFilename, stream = file)
+			p.sort_stats("calls").print_stats()
+	else:
+		main()
